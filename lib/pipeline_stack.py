f79cfe19a91e800d655aff97783f25a7315f4bef
hernandj <jose.hernandez@paramount.com>
2024-10-17 17:14:21 -0400

    Added the primary output directory for the synth shell step.

--- lib/pipeline_stack.py
+++ lib/pipeline_stack.py
@@ -141,7 +141,7 @@ class PipelineStack(cdk.Stack):
                     'python -m pip install -r requirements.txt --root-user-action=ignore',
                     'cdk synth'
                 ],
-                primary_output_directory='./cdk.out/',
+                primary_output_directory='/cdk.out',
             ),
             #cross_account_keys=True
         )
